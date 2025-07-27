1dba6845522bf5b367e52c835b69ae35cc447b08
Jair Poot <jairbalam12@gmail.com>
2025-07-27 07:58:39 -0600

    viteeee

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const allowedHostsFromEnv = process.env.VITE_ALLOWED_HOSTS?.split(',') || []
-
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
   preview: {
-    allowedHosts: allowedHostsFromEnv
+    host: true // ✅ permite cualquier dominio (como los de Render)
   }
 })
