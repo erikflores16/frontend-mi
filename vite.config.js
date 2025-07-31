5aa4913fcf34bac57dd470aa869eb9c3acecb8d2
erikflores16 <136302396+erikflores16@users.noreply.github.com>
2025-07-31 08:00:18 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
   preview: {
-    host: true // ✅ permite cualquier dominio (como los de Render)
+    allowedHosts: ['frontend-mi-12.onrender.com']
   }
 })
