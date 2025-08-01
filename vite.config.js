a1fd1b59efbf65c7c2082f6ae72117dd93a393e3
erikflores16 <136302396+erikflores16@users.noreply.github.com>
2025-08-01 00:49:28 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://backend-mi-1.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   preview: {
-    allowedHosts: ['frontend-mi-12.onrender.com']
-  }
+    allowedHosts: ['frontend-mi-12.onrender.com'],
+  },
 })
